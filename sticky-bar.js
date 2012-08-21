d3909fe491672149d4c559f0334c186e7132b5d7
sitesbyjoe <joe@sitesbyjoe.com>
2012-08-20 23:37:32 -0400

    added a bunch of comments and pulled some hard values into configurable settings.

--- sticky-bar.js
+++ sticky-bar.js
@@ -1,53 +1,81 @@
-$('.sidebar').removeClass('sticky');
+/*	The Sticky Bar
+ *	---------------------------------------------
+ *	A javascript object that makes your sidebar 
+ *	(or any element really) sticky so it follows 
+ *	the reader up an down the page.
+ *
+ *	@author Joseph R. B. Taylor - joe@sitesbyjoe.com
+ *
+ *	ToDos:
+ *	---------------------------------------------
+ *	+ Remove jQuery selectors - no dependencies!
+ *	+ Better variable names
+ *	+ Code Cleanup
+ *
+ */
+ 
+$('.sidebar').removeClass('sticky'); // Why the hell is this here?!?! It needs to die.
 
-var sticky = sticky || {};
+var stickyBar = {
 
-sticky.page = {
-        height: $(document).height(), 
-        viewportHeight: $(window).height(), 
-        sidebar: (($('.sidebar').height()) + 40)
-    };
+	// Change your setting here for the top and bottom measurements
+	stickyBar.config = {
+			top:  170;									// the height of your header, or just distance from the top of the page for the sidebar to start following
+			bottom: 454;								// the height of your footer, or just distance from the bottom for the sidebar to stop following
+			sidebarPaddingMargins: 40;	// the total vertical margin and padding that is on the sidebar - There's gotta be a way to get this automatically!!!
+			
+	};
 
+	// Some page variables StickyBar sets on its own
+	stickyBar.page = {
+      height: $(document).height(), 						// height of the entire document
+      viewportHeight: $(window).height(), 			// height of the viewport
+      sidebar: (($('.sidebar').height()) + stickyBar.config.sidebarPaddingMargins)	// height of the sidebar itself (plus 40px of padding)
+  };	
+	
+	stickyBar.zone = {  
+      top: stickyBar.config.top,  
+      bottom: (($(document).height() - stickyBar.config.bottom))
+  };
+	
+	// Spitting out some variables so you can see the measurement
+	console.log('-----stats-------');
+	console.log('doc height: ' + stickyBar.page.height);
+	console.log('top margin: ' + stickyBar.zone.top);
+	console.log('sidebar: ' + stickyBar.page.sidebar);
+	console.log('bottom margin: ' + stickyBar.zone.bottom);
+	
+	// Everytime the user scrolls we fire this function
+	$(document).scroll(function() {
+	
+	    var yLoc = $(document).scrollTop();
+	    var sidebarBottom = (Number(yLoc) + Number(stickyBar.page.sidebar));
+	
+	    //console.log(yLoc);
+	    if ((yLoc >= stickyBar.zone.top) && (sidebarBottom <= stickyBar.zone.bottom)) {
+	        //console.log('y sb top: ' + yLoc);
+	        //console.log('y sb bot: ' + sidebarBottom);
+	        //$('body').css('background', 'orange');
+	        $('.sidebar').attr('style', '').addClass('sticky'); // instead of this class which needs a stylesheet I should apply the styles directly!!!
 
-sticky.zone = {  
-        top: 170,  
-        //bottom: (($(document).height() - 454) - sticky.page.sidebar)
-        bottom: (($(document).height() - 454))
-    };
+	        //$('.sidebar').addClass('sticky');
+	    } else {
+	        
+	        //console.log('n sb top: ' + yLoc);
+	        //console.log('n sb bot: ' + sidebarBottom);
+	        //$('body').css('background', 'lime');
+	        //console.log($('.sidebar').offset.top());
+	        
+	        $('.sidebar').removeClass('sticky'); // instead of this class which needs a stylesheet I should apply the styles directly!!!
+	        
+	        if (yLoc > stickyBar.zone.top) {
+	            //console.log('npw top:' + (yLoc));
+	            //$('sidebar').css('margin-top',        
+	            $('.sidebar').css('position', 'absolute');
+	            $('.sidebar').css('bottom', '50px'); // gotta get rid of that fixed value!!!
+	            
+	        }
+	    }
+	});
 
-
-console.log('-----stats-------');
-console.log('doc height: ' + sticky.page.height);
-console.log('top margin: ' + sticky.zone.top);
-console.log('sidebar: ' + sticky.page.sidebar);
-console.log('bottom margin: ' + sticky.zone.bottom);
-
-$(document).scroll(function() {
-    var yLoc = $(document).scrollTop();
-    var sidebarBottom = (Number(yLoc) + Number(sticky.page.sidebar));
-
-    //console.log(yLoc);
-    //if ((yLoc >= sticky.zone.top) && ((Number(yLoc) + sticky.page.sidebar) <= sticky.zone.bottom)) {
-    if ((yLoc >= sticky.zone.top) && (sidebarBottom <= sticky.zone.bottom)) {
-        console.log('y sb top: ' + yLoc);
-        console.log('y sb bot: ' + sidebarBottom);
-        //$('body').css('background', 'orange');
-        $('.sidebar').attr('style', '');
-        $('.sidebar').addClass('sticky');
-    } else {
-        console.log('n sb top: ' + yLoc);
-        console.log('n sb bot: ' + sidebarBottom);
-        //$('body').css('background', 'lime');
-        //console.log($('.sidebar').offset.top());
-        
-        $('.sidebar').removeClass('sticky');
-        
-        if (yLoc > sticky.zone.top) {
-            //console.log('npw top:' + (yLoc));
-            //$('sidebar').css('margin-top',        
-            $('.sidebar').css('position', 'absolute');
-            $('.sidebar').css('bottom', '50px');
-            
-        }
-    }
-});
+};
